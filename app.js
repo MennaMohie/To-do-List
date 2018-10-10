2137019bb0c1916d928c1b7912de429abfd8a166
Menna Mohie <mennaxmohiex@gmail.com>
2018-10-10 19:31:39 +0200

    Added function newItem() to js

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 function newItem(){
-  
+  var li = document.createElement("li");
+  var value = document.getElementById("input").value;
+  var text = document.createTextNode(value);
+  li.appendChild(text);
+  li.class = "item";
+  if(value === ''){
+    alert('If you don\'t write something, then you don\'t need a list.!');
+  }
+  else{
+    document.getElementById("list").appendChild(li);
+  }
+  document.getElementById("input").value = "";
+}
+
+function selectItem(){
+
 }
